e8b2d45f76bf847061964ed0e8e5599972994d94
I AM VIJAY ࿐ <87687583+Viju60@users.noreply.github.com>
2021-10-08 14:35:33 +0530

    Update userinfo.py

--- TGNRobot/modules/userinfo.py
+++ TGNRobot/modules/userinfo.py
@@ -114,7 +114,7 @@ def hpmanager(user):
 
 def make_bar(per):
     done = min(round(per / 10), 10)
-    return "■" * done + "□" * (10 - done)
+    return "▣" * done + "■" * (10 - done)
 
 
 @run_async
@@ -518,30 +518,30 @@ def __user_info__(user_id):
 
 __help__ = """
 *Away from group*
- ❍ /afk <reason>*:* mark yourself as AFK(away from keyboard).
- ❍ brb <reason>*:* same as the afk command - but not a command.
+ ✪ /afk <reason>*:* mark yourself as AFK(away from keyboard).
+ ✪ brb <reason>*:* same as the afk command - but not a command.
 When marked as AFK, any mentions will be replied to with a message to say you're not available!
 
 *ID:*
- ❍ /id*:* get the current group id. If used by replying to a message, gets that user's id.
- ❍ /gifid*:* reply to a gif to me to tell you its file ID.
+ ✪ /id*:* get the current group id. If used by replying to a message, gets that user's id.
+ ✪ /gifid*:* reply to a gif to me to tell you its file ID.
 
 *Self addded information:* 
- ❍ /setme <text>*:* will set your info
- ❍ /me*:* will get your or another user's info.
+ ✪ /setme <text>*:* will set your info
+ ✪ /me*:* will get your or another user's info.
 *Examples:* 💡
- ➩ /setme I am a wolf.
- ➩ /me @username(defaults to yours if no user specified)
+ ✪ /setme I am a wolf.
+ ✪ /me @username(defaults to yours if no user specified)
 
 *Information others add on you:* 
- ❍ /bio*:* will get your or another user's bio. This cannot be set by yourself.
- ❍ /setbio <text>*:* while replying, will save another user's bio 
+ ✪ /bio*:* will get your or another user's bio. This cannot be set by yourself.
+ ✪ /setbio <text>*:* while replying, will save another user's bio 
 *Examples:* 💡
- ➩ /bio @username(defaults to yours if not specified).`
- ➩ /setbio This user is a wolf` (reply to the user)
+ ✪ /bio @username(defaults to yours if not specified).`
+ ✪ /setbio This user is a wolf` (reply to the user)
 
 *Overall Information about you:*
- ❍ /info*:* get information about a user. 
+ ✪ /info*:* get information about a user. 
  
 *What is that health thingy?*
  Come and see [HP System explained](https://t.me/OnePunchUpdates/192)
@@ -567,7 +567,7 @@ dispatcher.add_handler(GET_BIO_HANDLER)
 dispatcher.add_handler(SET_ABOUT_HANDLER)
 dispatcher.add_handler(GET_ABOUT_HANDLER)
 
-__mod_name__ = "Infos"
+__mod_name__ = "INFOS🧐"
 __command_list__ = ["setbio", "bio", "setme", "me", "info"]
 __handlers__ = [
     ID_HANDLER,
