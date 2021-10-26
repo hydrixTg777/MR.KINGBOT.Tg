49b491194b51c8aa819fee539ce1507a30a609f3
I AM VIJAY ࿐ <87687583+Viju60@users.noreply.github.com>
2021-10-26 06:42:57 +0530

    Update __main__.py

--- TGNRobot/__main__.py
+++ TGNRobot/__main__.py
@@ -74,7 +74,8 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """💫 HOI....♥️
-◈◈◈❰❰ MR.PROBOT ❱❱◈◈◈
+◈◈◈❰❰ [𓄂MR PROBOT࿐](http://t.me/PRO_VK_ROBOT) ❱❱◈◈◈
+┏━━━━━━━━━━━━━━━━┓🇴 🇵 ┗━━━━━━━━━━━━━━━━┛
 🟡 **I AM HERE TO HELP YOU TO MANAGE YOUR GROUPS AND I AM VERY POWERFULL BOT!!
 🟡YOU CAN FIND THE LIST OF AVALABLE COMMANDS WITH /help** [♥️](https://telegra.ph/file/79ad876457906a1137917.jpg)
 """
